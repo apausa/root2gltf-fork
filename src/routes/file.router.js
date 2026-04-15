744df13b8361cc338959a8769c4eeff67979e5df
pablo <74920898+apausa@users.noreply.github.com>
2026-04-15 15:13:54 +0200

    Implement multer

--- src/routes/file.router.js
+++ src/routes/file.router.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import multer from 'multer';
 
 import { parseFile } from '../controllers/file.controller';
 
 const fileRouter = express.Router();
+const upload = multer({ dest: '/tmp' });
 
-fileRouter.route('/').post(parseFile);
+fileRouter.route('/').post(upload.single('file'), parseFile);
 
 // eslint-disable-next-line import-x/prefer-default-export
 export { fileRouter };
