46e0a3b7fc4ecf4898a7cc4fa810c74669f35ba3
pablo <74920898+apausa@users.noreply.github.com>
2026-04-15 13:42:15 +0200

    Add JSON to config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,8 @@
-/**
- * THIS FILE WAS AUTO-GENERATED.
- * PLEASE DO NOT EDIT IT MANUALLY.
- * ===============================
- * IF YOU COPY THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
- */
-
 import path from 'node:path';
 
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import json from '@eslint/json';
 import { defineConfig } from 'eslint/config';
 import { configs, plugins, rules } from 'eslint-config-airbnb-extended';
 
@@ -44,4 +38,23 @@ export default defineConfig([
   ...jsConfig,
   // Node config
   ...nodeConfig,
+  // JSON config
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
 ]);
