f3becd9660d8f034b91903e57507cb5019085ae5
pablo <74920898+apausa@users.noreply.github.com>
2026-07-13 09:54:56 +0200

    Remove palette and avoid overriding the predefined colors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -13,29 +13,8 @@ export const SPHERE_NZ = 3;
 // jsroot build parameters
 export const GEO_GRAD_PER_SEGM = 360 / 24;
 export const BUILD_OPTIONS = {
-  dflt_colors: true, // assigns default ROOT colors to volumes that don't have an explicit fill color set
+  dflt_colors: false, // avoids overriding predefined colors
   vislevel: 10, // max depth of the geometry hierarchy to traverse and render
   numfaces: 10000000, // cap on the total number of triangle faces across the whole scene
   numnodes: 500000, // cap on the total number of visible nodes across the whole scene
 };
-
-// Color palette applied per subpart, cycled by index if there are more subparts than colors
-export const COLOR_PALETTE = [
-  "#fb2c36",
-  "#ff6900",
-  "#fe9a00",
-  "#f0b100",
-  "#7ccf00",
-  "#00c950",
-  "#00bc7d",
-  "#00bba7",
-  "#00b8db",
-  "#00a6f4",
-  "#2b7fff",
-  "#615fff",
-  "#8e51ff",
-  "#ad46ff",
-  "#e12afb",
-  "#f6339a",
-  "#ff2056",
-];
