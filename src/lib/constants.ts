552d1ee5631bf61c8f958ef3152acd84a1d47bbc
pablo <74920898+apausa@users.noreply.github.com>
2026-08-04 16:13:39 +0200

    Sort constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,12 +2,10 @@
 export const DEPTH = 3;
 
 // Visibility flags
-export const K_VIS_THIS = 0x80;
+export const K_VIS_ON_SCREEN = 0x80;
 export const K_VIS_DAUGHTER = 0x8;
 
-// jsroot types and properties
-export const T_GEO_SPHERE = "TGeoSphere";
-export const T_GEO_COMPOSITE_SHAPE = "TGeoCompositeShape";
+// jsroot properties
 export const T_GEO_B_BOX_IDENTITY_FIELDS = new Set([
   "fUniqueID",
   "fBits",
@@ -29,6 +27,8 @@ export const MATRIX_TYPES = new Set([
 // Sphere segment counts
 export const SPHERE_NSEG = 3;
 export const SPHERE_NZ = 3;
+export const T_GEO_SPHERE = "TGeoSphere";
+export const T_GEO_COMPOSITE_SHAPE = "TGeoCompositeShape";
 
 // jsroot build parameters
 export const GEO_GRAD_PER_SEGM = 360 / 30;
